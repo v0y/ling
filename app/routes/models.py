af51c2c806805c4c16093b33514e408f8a0c106a
cosiek <witold.majchrzak@21s.pl>
2013-11-19 22:03:36 +0100

    #37: routes model

--- app/routes/models.py
+++ app/routes/models.py
@@ -1,1 +1,14 @@
+# encoding: utf-8
+
+from django.db import models
+from jsonfield.fields import JSONField
+
+from app.shared.models import CreatedAtMixin
+
+class Route(CreatedAtMixin):
+    tracks_json = JSONField(default='[]')
+
+    start_time = models.DateTimeField(auto_now=False, null=True, verbose_name=u'Czas rozpoczęcia trasy')
+    finish_time = models.DateTimeField(auto_now=False, null=True, verbose_name=u'Czas zakończenia trasy')
+    length = models.IntegerField(default=0, verbose_name=u'Długość trasy')
 

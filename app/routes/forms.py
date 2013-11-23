a65fed99cb1d473218f9553cef89537619abf1cb
cosiek <witold.majchrzak@21s.pl>
2013-11-24 00:04:42 +0100

    #38: assign route to workout through workout form

--- app/routes/forms.py
+++ app/routes/forms.py
@@ -1,8 +1,17 @@
 # encoding: utf-8
 
 from django import forms
-from django.forms import Form
+
+from .models import Route
 
 
-class GPXForm(Form):
-    gpx_file = forms.FileField()
+class RouteIdMixin(forms.Form):
+    route_id = forms.IntegerField(required=False, widget=forms.HiddenInput())
+
+    def clear_route_id(self):
+        if self.cleaned_data['route_id']:
+            id = self.cleaned_data['route_id']
+            if Route.objects.filter(id=id).exists():
+                return id
+            else:
+                raise forms.ValidationError("Non-existent route id supplied.")

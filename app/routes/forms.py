9050de3934af2d2901c8b3d267cad9a9c1d78976
cosiek <witold.majchrzak@21s.pl>
2013-12-09 22:14:19 +0100

    ^ dziedziczenie model formów jest do dupy

--- app/routes/forms.py
+++ app/routes/forms.py
@@ -1,14 +1,11 @@
 # encoding: utf-8
 
 from django import forms
-from django.forms.models import ModelForm
 
 from .models import Route
 
 
-class RouteIdMixin(ModelForm):
-    route_id = forms.IntegerField(required=False, widget=forms.HiddenInput())
-
+class RouteIdMixin():
     def clear_route_id(self):
         if self.cleaned_data['route_id']:
             id = self.cleaned_data['route_id']
